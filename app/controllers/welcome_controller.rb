d26efa6a6c45f266d02fd9f98fb17bbe653f41a9
datapike <datapike@gmail.com>
2016-03-23 01:59:11 +0000

    balance updating working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
    
    def index
      @total = Account.find(1).balance
+     @total_date = Account.find(1).balance_date
    end
     
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,2 @@
 Time::DATE_FORMATS[:day_month_year] = '%b %d %Y'
+Time::DATE_FORMATS[:day_only] = '%b'
